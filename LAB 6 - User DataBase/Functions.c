5d04cf553c1170a89dce74411563eab5d801258e
Ravela Smyth <RavSmy@users.noreply.github.com>
2017-05-03 16:19:03 -0700

    Update Functions.c

--- LAB 6 - User DataBase/Functions.c
+++ LAB 6 - User DataBase/Functions.c
@@ -1,18 +1,31 @@
+/*
+	"Functions.c"
+
+	Ravela Smyth
+	LAB 6B, 7B
+	CS 539
+	5/9/2017
+
+*/
+
 #include "Functions.h"
 #include <stdio.h>
 #include <stdlib.h>
 
+/*Creates up to 50 structures*/
 void create_data(user_data *users[]) 
 {
 	char new_user[100]; 
 	int i = 0; 
 	user_data *temp;
 	
+	/*Gets input until no info left*/
 	while (gets(new_user) != NULL && i < 50) 
 	{
-		
+		/*Dynamically Allocates new structure, then copies to array of pointers to structures*/
 		temp = (user_data*)malloc(sizeof(user_data));
-
+		
+		/*Get info from file*/
 		strcpy(temp->name, new_user);
 		gets(temp->address);
 		gets(temp->city);
@@ -24,10 +37,12 @@ void create_data(user_data *users[])
 	users[i] = NULL;
 }
 
+/*Sorts data according the zipcode*/
 void sort_data(user_data *users[]) 
 {
 	user_data *temp; 
-
+	
+	/*Sorts smallest number to beginning*/
 	for (int i = 0; users[i] != NULL; i++) 
 		for (int j = i+1; users[j] != NULL; j++)
 			if (strcmp(users[i]->zipcode, users[j]->zipcode) > 0) 
@@ -38,12 +53,14 @@ void sort_data(user_data *users[])
 			}
 }
 
+/*Outputs data*/
 void print_data(user_data *users[]) 
 {
 	for (int i = 0; users[i] != NULL; i++)
 		printf("%s\n%s\n%s\n%s\n", users[i]->name, users[i]->address, users[i]->city, users[i]->zipcode);
 }
 
+/*Frees dynamically allocated memory*/
 void free_mem(user_data *users[]) 
 {
 	for (int i = 0; users[i] != NULL; i++) 
